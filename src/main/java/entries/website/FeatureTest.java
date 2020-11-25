35242b4aa3137bfbe931cf6b0e23485292ae0ef2
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-11-25 19:15:52 +0100

    DataTables example done.

--- src/main/java/entries/website/FeatureTest.java
+++ src/main/java/entries/website/FeatureTest.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "Test", urlPatterns = { "/Test" } )
@@ -19,4 +20,14 @@ public class FeatureTest extends BaseServlet {
         
     }
     
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String valFromTable = req.getParameter("ValOne");
+    
+        HttpSession session = req.getSession();
+        
+        session.setAttribute("valSet", valFromTable);
+    
+        render("Startside", "/WEB-INF/pages/test.jsp", req, resp);
+    }
 }
