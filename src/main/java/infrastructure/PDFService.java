32b3b2e67b86672781e72fa9c8a97e5d22eb9551
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-12-09 10:35:31 +0100

    Convert SVG to PNG PDF template fixed

--- src/main/java/infrastructure/PDFService.java
+++ src/main/java/infrastructure/PDFService.java
@@ -2,15 +2,28 @@ package infrastructure;
 import api.FileService;
 import api.Utils;
 import api.exceptions.PDFNotCreated;
+import com.itextpdf.html2pdf.ConverterProperties;
 import domain.material.materials.Tree;
 import domain.order.Order;
 
 import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 
 import com.itextpdf.html2pdf.HtmlConverter;
 import domain.partslist.Part;
 import infrastructure.exceptions.PDFNotFound;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.slf4j.Logger;
+import org.xml.sax.InputSource;
+
+import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -19,25 +32,34 @@ public class PDFService implements FileService {
     private static final String templateFileName = "pdftemplate.html";
     private final String fileDir = System.getProperty("java.io.tmpdir");
     
-    private static final String drawingsPath = "testfiles/";
-    
     private String fillTemplate(Order order, String svgSide, String svgTop){
         String templateSrc = Utils.fileToString("pdf/"+templateFileName);
         
         templateSrc = templateSrc.replace("$$ORDRENUMMER$$", String.valueOf(order.getId()));
+    
+        String svgTopName = "/" + order.getUuid() + "-top.png";
+        String svgSideName = "/" + order.getUuid() + "-side.png";
         
-        //TODO : Fix svg errors.
-        templateSrc = templateSrc.replace("$$sideDrawing$$", svgSide);
-        templateSrc = templateSrc.replace("$$topDrawing$$", svgTop);
+        String topPng = "";
+        String sidePng = "";
+    
+        try {
+            topPng = "<img src=\"" + convertSVGtoPNG(svgTopName, svgTop) + "\" />";
+            sidePng = "<img src=\"" + convertSVGtoPNG(svgSideName, svgSide) + "\" />";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
+        templateSrc = templateSrc.replace("$$sideDrawing$$", sidePng);
+        templateSrc = templateSrc.replace("$$topDrawing$$", topPng);
         
         StringBuilder parts = new StringBuilder();
         String partRowTemplate = "<tr>\n" +
-                "                <td>$$MATERIAL$$</td>\n" +
-                "                <td>$$LENGTH$$</td>\n" +
-                "                <td>$$AMOUNT$$</td>\n" +
-                "                <td>$$UNIT$$</td>\n" +
-                "                <td>$$DESCRIPTION$$</td>\n" +
+                "                <td class=\"tg-0lax\">$$MATERIAL$$</td>\n" +
+                "                <td class=\"tg-0lax\">$$LENGTH$$</td>\n" +
+                "                <td class=\"tg-0lax\">$$AMOUNT$$</td>\n" +
+                "                <td class=\"tg-0lax\">$$UNIT$$</td>\n" +
+                "                <td class=\"tg-0lax\">$$DESCRIPTION$$</td>\n" +
                 "            </tr>";
         
         for(Part p: order.getCarport().getPartslist().getPartList()){
@@ -75,7 +97,15 @@ public class PDFService implements FileService {
             newPdf.createNewFile(); // if file already exists will do nothing
             FileOutputStream fileOutput = new FileOutputStream(newPdf);
     
-            HtmlConverter.convertToPdf(htmlString, fileOutput);
+            ConverterProperties converterProperties = new ConverterProperties();
+            URL resourcesUrl = getClass().getClassLoader().getResource("/pdf/");
+            try {
+                converterProperties.setBaseUri(resourcesUrl.toURI().toString());
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+    
+            HtmlConverter.convertToPdf(htmlString, fileOutput, converterProperties);
     
             log.info("PDF Generated: " + newPdf.getAbsolutePath());
             
@@ -100,5 +130,36 @@ public class PDFService implements FileService {
         } catch (Exception e) {
             throw new PDFNotFound(e);
         }
+    }
+    
+    @Override
+    public String convertSVGtoPNG(String fileName, String svgSource) throws Exception {
+        
+        InputStream inputStream = new ByteArrayInputStream(svgSource.getBytes(StandardCharsets.UTF_8));
+        
+        TranscoderInput input = new TranscoderInput(inputStream);
+    
+        File dir = new File(fileDir);
+        dir.mkdirs();
+    
+        File newPdf = new File(fileDir + fileName);
+        newPdf.createNewFile(); // if file already exists will do nothing
+        
+        
+        OutputStream png_ostream = new FileOutputStream(newPdf);
+        
+        TranscoderOutput png_output = new TranscoderOutput(png_ostream);
+        
+        PNGTranscoder transcoder = new PNGTranscoder();
+//        transcoder.addTranscodingHint(PNGTranscoder.KEY_MAX_HEIGHT, 1500);
+//        transcoder.addTranscodingHint(PNGTranscoder.KEY_MAX_WIDTH, 1500);
+        
+        transcoder.transcode(input, png_output);
+        
+        png_ostream.flush();
+        png_ostream.close();
+    
+        return newPdf.getAbsolutePath();
+        
     }
 }
--- src/main/webapp/WEB-INF/includes/header.jsp
+++ src/main/webapp/WEB-INF/includes/header.jsp
@@ -2,7 +2,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@page pageEncoding="UTF-8"%>
 <!--    Header image-->
-<div>
+<div class="row">
     <a href="${pageContext.request.contextPath}"><img src="${pageContext.request.contextPath}/images/FOG.jpg" class="img-fluid" alt="Responsive image"></a>
 </div>
 <%@include file="navigation.jsp"%>
