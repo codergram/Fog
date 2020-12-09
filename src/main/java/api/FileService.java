36316c0aa8d1862c0e3c4e321923554dffb076ee
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-12-09 10:35:31 +0100

    Convert SVG to PNG PDF template fixed

--- src/main/java/api/FileService.java
+++ src/main/java/api/FileService.java
@@ -5,8 +5,10 @@ import domain.order.Order;
 import infrastructure.exceptions.PDFNotFound;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public interface FileService {
     File generatePdf(Order order, String svgSide, String svgTop) throws PDFNotCreated;
     File getPdf(String filename) throws PDFNotFound;
+    String convertSVGtoPNG(String fileName, String svgSource) throws Exception;
 }
