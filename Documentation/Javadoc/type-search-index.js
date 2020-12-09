08f64ebf53870640c1d89f4d4120f53c3fbb89ab
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-12-09 20:36:50 +0100

    cleanup

--- Documentation/Javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"api","l":"Api"},{"p":"api.exceptions","l":"ApiError"},{"p":"api.exceptions","l":"EmailNotSent"},{"p":"api","l":"EmailService"},{"p":"api","l":"FileService"},{"p":"api.exceptions","l":"PDFNotCreated"},{"p":"api","l":"Utils"}]
