43dea1098576563c606e12e0d592cfc052280d0f
ariktwena <ariktwena@gmail.com>
2020-12-10 15:26:42 +0100

    test

--- src/test/java/domain/svg/SVGTopTest.java
+++ src/test/java/domain/svg/SVGTopTest.java
@@ -9,6 +9,7 @@
 package domain.svg;
 
 import domain.carport.Carport;
+import domain.carport.Carport.Roof;
 import domain.carport.shed.Shed;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +24,7 @@ class SVGTopTest {
     double shedLength = 210;
     double shedWidth = width - 75.0;
     Shed shed = new Shed(shedLength, shedWidth);
-    Carport carport = new Carport(780, 600, Carport.Roof.Flat, shed);
+    Carport carport = new Carport(length, width, Carport.Roof.Flat, shed);
 
     String expectedResult =
         "<svg version=\"1.1\"\n"
@@ -35,6 +36,29 @@ class SVGTopTest {
     SVGTop svgTop = new SVGTop(carport, false);
 
     //        System.out.println(svgTop.getSvgTop());
+
+    assertTrue(svgTop.getSvgTop().startsWith(expectedResult));
+  }
+
+  @Test
+  void testSVGTop1() {
+    double length = 600;
+    double width = 300;
+    double shedLength = 210;
+    double shedWidth = width - 75.0;
+    Shed shed = new Shed(shedLength, shedWidth);
+    Carport carport = new Carport(length, width, Roof.Peak, shed);
+
+    String expectedResult =
+        "<svg version=\"1.1\"\n"
+            + "     xmlns=\"http://www.w3.org/2000/svg\"\n"
+            + "     xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n"
+            + "     height=\"100%\"  width=\"100%\" viewBox=\"0 0 700.0 400.0\"\n"
+            + "     preserveAspectRatio=\"xMinYMin\" ";
+
+    SVGTop svgTop = new SVGTop(carport, false);
+
+          System.out.println(svgTop.getSvgTop());
 
     assertTrue(svgTop.getSvgTop().startsWith(expectedResult));
   }
